b5d6ae97f51a77603432a2a03caa811302e95bc1
Rachel Carvalho <rachel.carvalho@gmail.com>
2017-06-01 13:12:12 -0400

    make class immutable

--- clock.rb
+++ clock.rb
@@ -7,41 +7,25 @@ class Clock
     new(hours, minutes)
   end
 
-  attr_reader :hours, :minutes
-
   def initialize(hours, minutes)
-    self.minutes = minutes
-    self.hours   = hours
+    @hours   = (hours + minutes / 60) % 24
+    @minutes = minutes % 60
+    self.freeze
   end
 
   def to_s
-    "#{format(hours)}:#{format(minutes)}"
+    "#{format(@hours)}:#{format(@minutes)}"
   end
 
   def +(minutes)
-    self.tap { self.minutes += minutes }
+    Clock.new(@hours, @minutes + minutes)
   end
 
   def ==(other)
     to_s == other.to_s
   end
 
-  def hours=(value)
-    return @hours = value if value.nil?
-    @hours = (value + @full_minutes / 60) % 24
-  end
-
-  def minutes=(value)
-    @full_minutes = value
-    @minutes      = value % 60
-    update_hours
-  end
-
   private
-
-  def update_hours
-    self.hours = self.hours
-  end
 
   def format(value)
     sprintf '%02d', value
