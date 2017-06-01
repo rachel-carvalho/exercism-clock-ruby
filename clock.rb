c63923efb7d51b866f1d400d17e4bdd4b1ac15a5
Rachel Carvalho <rachel.carvalho@gmail.com>
2017-06-01 12:45:54 -0400

    initial implementation

---
+++ clock.rb
@@ -0,0 +1,46 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Clock
+  def self.at(hours, minutes)
+    new(hours, minutes)
+  end
+
+  attr_reader :hours, :minutes
+
+  def initialize(hours, minutes)
+    self.minutes = minutes
+    self.hours   = hours
+  end
+
+  def to_s
+    "#{format(hours)}:#{format(minutes)}"
+  end
+
+  def +(minutes)
+    self.minutes += minutes
+    self
+  end
+
+  def ==(other)
+    to_s == other.to_s
+  end
+
+  def hours=(value)
+    return if value.nil?
+    @hours = (value + (@full_minutes / 60.0).floor) % 24
+  end
+
+  def minutes=(value)
+    @full_minutes = value
+    @minutes      = value % 60
+    self.hours    = self.hours
+  end
+
+  private
+
+  def format(value)
+    sprintf '%02d', value
+  end
+end
